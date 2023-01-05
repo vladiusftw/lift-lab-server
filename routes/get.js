62ba2de740981d3bf362d6350185232de729a5dd
Abdul Rahman Al Sbeinaty <abdul@lunapr.io>
2023-01-05 04:19:14 +0400

    fixes

--- routes/get.js
+++ routes/get.js
@@ -12,7 +12,7 @@ const verify = require("../verify");
 
 router.get("/login", login);
 router.get("/user", verify, getUserByEmail);
-router.get("/user-equipment", verify, getUserEquipment);
+router.get("/user-equipments", verify, getUserEquipment);
 router.get("/equipments", verify, getEquipments);
 router.get("/user-exercises", verify, getUserExercises);
 router.get("/user-available-exercises", verify, getUserAvailableExercises);
